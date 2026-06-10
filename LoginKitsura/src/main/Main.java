a6f42be6c93b4b8c0e66815c44eeca1dc485ec11
Mario <mariotumax@emilianisomascos.edu.gt>
2026-06-10 10:57:59 -0600

    Cambio de interfaces

--- LoginKitsura/src/main/Main.java
+++ LoginKitsura/src/main/Main.java
@@ -1,10 +1,22 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
         Conexion conectar = new Conexion();
-        conectar.getConnection();
-        IniciarSesion uevo = new IniciarSesion();
-    }   
+
+        if (conectar.getConnection() != null) {
+            MenuPrincipal nuevo = new MenuPrincipal();
+            nuevo.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Error al conectar con la base de datos",
+                    "Error de conexión",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
 }
