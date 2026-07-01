c62b0668bf58516d906bf83b82da81907b64fbf7
Brandon Martinezzz <brandonmartinez@emilianisomascos.edu.gt>
2026-07-01 10:09:20 -0600

    Correccion de constructor de PantallaDificultad

--- LoginKitsura/src/main/conexion/Conexion.java
+++ LoginKitsura/src/main/conexion/Conexion.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Conexion {
     private static String User = "root";
-    private static String Password = "admin";
+    private static String Password = "123456789";
  private static String Url =
     "jdbc:mysql://localhost:3306/KITSURA_DB"
     + "?useSSL=false"
