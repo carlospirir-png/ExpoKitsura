01411ab5af265c50013cfa2a3e31ab5eedcbcbf7
Alessandra Juarez <biancajuarez@emilianisomascos.edu.gt>
2026-07-03 22:21:16 -0600

    Interfaces Admin

--- LoginKitsura/src/main/Administrador/TiempoAdmin.java
+++ LoginKitsura/src/main/Administrador/TiempoAdmin.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-// AUN ME FALTA MODIFICAR ERROREEEEEES
+
 public class TiempoAdmin extends JFrame {
 
     private FondoPanel fondo;
