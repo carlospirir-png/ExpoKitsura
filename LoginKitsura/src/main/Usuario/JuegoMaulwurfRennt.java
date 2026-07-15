c73398202abbdb7852fb8e65543e30ba0c923d74
Mario <mariotumax@emilianisomascos.edu.gt>
2026-07-15 10:15:59 -0600

    Cambio de color en difiultad 3 de MaulwurfRennt

--- LoginKitsura/src/main/Usuario/JuegoMaulwurfRennt.java
+++ LoginKitsura/src/main/Usuario/JuegoMaulwurfRennt.java
@@ -158,7 +158,7 @@ public class JuegoMaulwurfRennt implements JuegoBase {
                 vista.actualizarNivel(3);
                 vista.actualizarDificultad("Difícil");
                 vista.mostrarTopos(7);
-                vista.cambiarColorFondo(new Color(255, 180, 80));
+                vista.cambiarColorFondo(new Color(238, 151, 151));
                 vista.actualizarCategoria(obtenerNombreCategoria());
                 break;
         }
